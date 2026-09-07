08a17e80dd6b2d8bee2f73e240ad5288048c4b2f
Stolyarovmn <maxim.n.stolyarov@gmail.com>
2026-09-07 12:33:43 +0300

    refactor(ui): delete rail shadow patch

--- app/content/rail-prelude.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  if (window !== window.top || globalThis.__atnRailShadowPrelude) return;
-  globalThis.__atnRailShadowPrelude = true;
-  const original = Element.prototype.attachShadow;
-  Element.prototype.attachShadow = function(init){
-    if (this?.id === "app-tower-next-host") return original.call(this,{...init,mode:"open"});
-    return original.call(this,init);
-  };
-})();
